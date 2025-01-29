4fcbcbcdffe1a6a868bf1493128e8ca21d2ccb32
Andy Ford <andy.ford@snowflake.com>
2025-01-29 19:54:47 +0000

    Go go go

--- deployment/current/JIRAREF_create_new_database_in_staging.sql
+++ deployment/current/JIRAREF_create_new_database_in_staging.sql
@@ -1,2 +1,8 @@
-execute immediate from '@git_repo/branches/main/devops/account_level/database_staging.sql'
-using (env => '{{env}}');
+begin
+
+  use database {{git_db}};
+  use schema  {{git_sc}};
+  execute immediate from '@git_repo/branches/main/devops/account_level/database_staging.sql'
+  using (env => '{{env}}');
+
+end;
