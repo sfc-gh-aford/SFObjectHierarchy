d32010df4261b40fa3c5db4c761d743d7dddf6d0
Andy Ford <andy.ford@snowflake.com>
2025-01-29 16:21:34 +0000

    Added extra statement

--- deployment/current/JIRAREF_create_core_layer.sql
+++ deployment/current/JIRAREF_create_core_layer.sql
@@ -1,8 +1,12 @@
 begin
-execute immediate from '@git_repo/branches/main/devops/account_level/database_core.sql'
-using (env => '{{env}}');
-execute immediate from '@git_repo/branches/main/devops/database_level/schema_core_preparation.sql'
-using (env => '{{env}}');
---execute immediate from '@git_repo/branches/main/devops/database_level/schema_core_transformation.sql'
---using (env => '{{env}}');
+
+  execute immediate from '@git_repo/branches/main/devops/account_level/database_core.sql'
+  using (env => '{{env}}');
+
+  execute immediate from '@git_repo/branches/main/devops/database_level/schema_core_preparation.sql'
+  using (env => '{{env}}');
+  
+  execute immediate from '@git_repo/branches/main/devops/database_level/schema_core_transformation.sql'
+  using (env => '{{env}}');
+  
 end;
