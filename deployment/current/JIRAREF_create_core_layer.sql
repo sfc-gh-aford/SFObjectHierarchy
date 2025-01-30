0fb450569457ba849f3960ea0fa842370c8cc299
Andy Ford <andy.ford@snowflake.com>
2025-01-30 07:38:21 +0000

    Changes to calling scripts

--- deployment/current/JIRAREF_create_core_layer.sql
+++ deployment/current/JIRAREF_create_core_layer.sql
@@ -2,13 +2,16 @@ begin
 
   use database {{git_db}};
   use schema  {{git_sc}};
-  
   execute immediate from '@git_repo/branches/main/devops/account_level/database_core.sql'
   using (env => '{{env}}');
 
+  use database {{git_db}};
+  use schema  {{git_sc}};
   execute immediate from '@git_repo/branches/main/devops/database_level/schema_core_preparation.sql'
   using (env => '{{env}}');
   
+  use database {{git_db}};
+  use schema  {{git_sc}};
   execute immediate from '@git_repo/branches/main/devops/database_level/schema_core_transformation.sql'
   using (env => '{{env}}');
   
