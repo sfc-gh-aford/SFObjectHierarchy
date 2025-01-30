6f4b26f39b2bd310a27e665ccebd0030c61ee805
Andy Ford <andy.ford@snowflake.com>
2025-01-30 07:26:36 +0000

    New YAML file for production deployment

--- deployment/current/JIRAREF_create_core_layer.sql
+++ deployment/current/JIRAREF_create_core_layer.sql
@@ -1,5 +1,8 @@
 begin
 
+  use database {{git_db}};
+  use schema  {{git_sc}};
+  
   execute immediate from '@git_repo/branches/main/devops/account_level/database_core.sql'
   using (env => '{{env}}');
 
