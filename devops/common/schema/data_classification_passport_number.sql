a74323dd0deecad55fbb77be4e31d4d2ba5e240f
Andy Ford <andy.ford@snowflake.com>
2025-05-28 14:55:54 +0100

    move to create or replace for tag creation

--- devops/common/schema/data_classification_passport_number.sql
+++ devops/common/schema/data_classification_passport_number.sql
@@ -1,10 +1,10 @@
 -- script to classify passport number in raw_data database
 
 -- create a tag for passport number
-create or alter tag common.governance.passport_number_tag;
+create or replace tag common.governance.passport_number_tag;
 
 -- create masking poilicy to mask passport number
-create or alter masking policy common.governance.passport_number_mask
+create or replace masking policy common.governance.passport_number_mask
 as (val string) returns string ->
   case
     when system$get_tag_on_current_column('common.governance.passport_number_tag') = 'visible' then val
