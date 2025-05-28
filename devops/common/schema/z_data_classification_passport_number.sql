bef35c286adef67e91f7cd48a02c99fa5bcf79de
Andy Ford <andy.ford@snowflake.com>
2025-05-28 15:20:14 +0100

    changed order

--- devops/common/schema/z_data_classification_passport_number.sql
+++ devops/common/schema/z_data_classification_passport_number.sql
@@ -1,5 +1,8 @@
 -- script to classify passport number in raw_data database
 
+-- create a tag for passport number
+create or alter tag common.governance.passport_number_tag;
+
 -- create masking poilicy to mask passport number
 create or replace masking policy common.governance.passport_number_mask
 as (val string) returns string ->
@@ -7,9 +10,6 @@ as (val string) returns string ->
     when system$get_tag_on_current_column('common.governance.passport_number_tag') = 'visible' then val
     else '***MASKED***'
   end;
-
--- create a tag for passport number
-create or alter tag common.governance.passport_number_tag;
 
 -- associate tag with masking policy for passport number
 alter tag common.governance.passport_number_tag set masking policy common.governance.passport_number_mask;
