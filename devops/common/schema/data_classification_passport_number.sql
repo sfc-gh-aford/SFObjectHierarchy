034b30a69653a2c0beaa5e2ece50788726b433f0
Andy Ford <andy.ford@snowflake.com>
2025-05-28 14:49:39 +0100

    change to create or alter masking policy

--- devops/common/schema/data_classification_passport_number.sql
+++ devops/common/schema/data_classification_passport_number.sql
@@ -4,7 +4,7 @@
 create or alter tag common.governance.passport_number_tag;
 
 -- create masking poilicy to mask passport number
-create or replace masking policy common.governance.passport_number_mask
+create or alter masking policy common.governance.passport_number_mask
 as (val string) returns string ->
   case
     when system$get_tag_on_current_column('common.governance.passport_number_tag') = 'visible' then val
