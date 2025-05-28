57b43423d792749b0cecafc5910f31c11aa3bbbe
Andy Ford <andy.ford@snowflake.com>
2025-05-28 16:10:59 +0100

    latest 2

--- devops/common/schema/data_classification_passport_number.sql
+++ devops/common/schema/data_classification_passport_number.sql
@@ -5,6 +5,9 @@ alter table rawdata.traveller.voucher
     modify column v_passport_no 
     unset tag common.governance.passport_number_tag;
 
+-- create a tag for passport number
+create or replace tag common.governance.passport_number_tag;
+
 -- create masking poilicy to mask passport number
 create or replace masking policy common.governance.passport_number_mask
 as (val string) returns string ->
@@ -12,9 +15,6 @@ as (val string) returns string ->
     when system$get_tag_on_current_column('common.governance.passport_number_tag') = 'visible' then val
     else '***MASKED***'
   end;
-
--- create a tag for passport number
-create or alter tag common.governance.passport_number_tag;
 
 -- associate tag with masking policy for passport number
 alter tag common.governance.passport_number_tag set masking policy common.governance.passport_number_mask;
