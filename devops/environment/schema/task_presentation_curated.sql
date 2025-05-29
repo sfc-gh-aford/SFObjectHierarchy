666bd443344b45004693238f81b110b06edae597
Andy Ford <andy.ford@snowflake.com>
2025-05-29 08:03:37 +0100

    resume test task in presentation.curated

--- devops/environment/schema/task_presentation_curated.sql
+++ devops/environment/schema/task_presentation_curated.sql
@@ -1,7 +1,12 @@
--- script to create env_staging.appflow schema
+-- script to create tasks in env_presentation.curated schema
 -- env variable to be provided by snowflake-deployment.yml
+
+-- create test task
 create or alter task {{env}}_presentation.curated.test_task
   schedule = 'USING CRON 0 0 1 * * UTC' 
   user_task_managed_initial_warehouse_size = 'XSMALL'
   as
     select current_timestamp;
+
+-- resume test task
+alter task {{env}}_presentation.curated.test_task resume;
